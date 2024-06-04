1c1654e1d2c57b103abe318dd8295e0e06c856d9
allexkap <aekpstn@gmail.com>
2024-06-05 02:24:30 +0300

    add prog

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use rdev::{listen, simulate, EventType, Key};
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    let (tx, rx) = mpsc::channel();
+    thread::spawn(move || {
+        listen(move |event| {
+            tx.send(match event.event_type {
+                EventType::KeyPress(Key::KeyA) => 0,
+                EventType::KeyRelease(Key::KeyA) => 1,
+                _ => return,
+            })
+            .unwrap();
+        })
+        .unwrap();
+    });
+    for event in rx {
+        simulate(&match event {
+            1 => EventType::KeyPress(Key::KeyX),
+            0 => EventType::KeyRelease(Key::KeyX),
+            _ => continue,
+        })
+        .unwrap();
+    }
 }
