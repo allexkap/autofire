c69bd42ad3c377a6b617ba1ebccfed2ebcccea9e
allexkap <aekpstn@gmail.com>
2024-06-18 15:25:24 +0300

    add xinput dll patcher

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,66 @@
-use enigo::{self, Direction, Keyboard};
-use gilrs::{self, Event, EventType};
+use std::io::Write;
+
+const DEFAULT_PATH: &str = "C:/Program Files (x86)/Steam/steamapps/common/Cuphead";
+const BYTECODE: [[u8; 25]; 2] = [
+    [
+        0x8B, 0xC3, 0x48, 0x8B, 0x5C, 0x24, 0x50, 0x48, 0x83, 0xC4, 0x30, 0x41, 0x5E, 0x5F, 0x5E,
+        0xC3, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC, 0xCC,
+    ],
+    [
+        0xB8, 0xFF, 0x00, 0x00, 0x00, 0x66, 0x31, 0x47, 0x07, 0x8B, 0xC3, 0x48, 0x8B, 0x5C, 0x24,
+        0x50, 0x48, 0x83, 0xC4, 0x30, 0x41, 0x5E, 0x5F, 0x5E, 0xC3,
+    ],
+];
+
+fn find_all<T: PartialEq>(data: &[T], req: &[T]) -> Vec<usize> {
+    (0..data.len() - req.len() + 1)
+        .filter(|&i| data[i..i + req.len()] == req[..])
+        .collect()
+}
+
+fn substitute<T: Clone>(data: &mut [T], req: &[T], i: usize) {
+    data[i..i + req.len()]
+        .iter_mut()
+        .zip(req.iter())
+        .for_each(|(d, b)| *d = b.clone());
+}
+
+fn pause() {
+    std::process::Command::new("cmd.exe")
+        .arg("/c")
+        .arg("pause")
+        .status()
+        .unwrap();
+}
+
+fn patch() -> std::io::Result<usize> {
+    let mut data = std::fs::read("C:/Windows/System32/XInput1_4.dll")?;
+    let mut path = String::new();
+
+    println!("default path {DEFAULT_PATH}");
+    print!("custom path or nothing: ");
+    std::io::stdout().flush()?;
+    std::io::stdin().read_line(&mut path)?;
+    path = path.lines().next().unwrap().into();
+    if path.len() == 0 {
+        path = DEFAULT_PATH.into();
+    }
+    path.push_str("/XInput1_4.dll");
+
+    let indexes = find_all(&data, &BYTECODE[0]);
+    if indexes.len() == 1 {
+        substitute(&mut data, &BYTECODE[1], indexes[0]);
+        std::fs::write(path, data)?;
+    }
+    Ok(indexes.len())
+}
 
 fn main() {
-    let mut gilrs = gilrs::Gilrs::new().unwrap();
-    let mut enigo = enigo::Enigo::new(&enigo::Settings::default()).unwrap();
-
-    let mut hold = gilrs.gamepads().count() != 0;
-    while let Some(Event { event, .. }) = gilrs.next_event_blocking(None) {
-        hold = match event {
-            EventType::Disconnected => false,
-            EventType::Connected => true,
-            EventType::ButtonPressed(gilrs::Button::LeftTrigger2, _)
-            | EventType::ButtonPressed(gilrs::Button::RightTrigger2, _)
-            | EventType::ButtonReleased(gilrs::Button::RightTrigger2, _) => !hold,
-            _ => continue,
-        };
-        let dir = if hold {
-            Direction::Press
-        } else {
-            Direction::Release
-        };
-        enigo.key(enigo::Key::X, dir).unwrap();
+    match patch() {
+        Err(err) => println!("{}", err),
+        Ok(0) => println!("pattern not found"),
+        Ok(1) => println!("successfully"),
+        Ok(_) => println!("multiple matches"),
     }
+    pause();
 }
