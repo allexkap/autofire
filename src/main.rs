0508d8c68e1fc746b8d7301ab57228c56ac1b9e5
allexkap <aekpstn@gmail.com>
2024-06-24 00:24:10 +0300

    add egui gui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
-use std::io::Write;
+use eframe::egui;
 
+const DLL_PATH: &str = "C:/Windows/System32/XInput1_4.dll";
 const DEFAULT_PATH: &str = "C:/Program Files (x86)/Steam/steamapps/common/Cuphead";
 const BYTECODE: [[u8; 25]; 2] = [
     [
@@ -25,42 +26,68 @@ fn substitute<T: Clone>(data: &mut [T], req: &[T], i: usize) {
         .for_each(|(d, b)| *d = b.clone());
 }
 
-fn pause() {
-    std::process::Command::new("cmd.exe")
-        .arg("/c")
-        .arg("pause")
-        .status()
-        .unwrap();
+struct AutofireApp {
+    path: String,
+    msg: String,
 }
 
-fn patch() -> std::io::Result<usize> {
-    let mut data = std::fs::read("C:/Windows/System32/XInput1_4.dll")?;
-    let mut path = String::new();
-
-    println!("default path {DEFAULT_PATH}");
-    print!("custom path or nothing: ");
-    std::io::stdout().flush()?;
-    std::io::stdin().read_line(&mut path)?;
-    path = path.lines().next().unwrap().into();
-    if path.len() == 0 {
-        path = DEFAULT_PATH.into();
+impl Default for AutofireApp {
+    fn default() -> Self {
+        AutofireApp {
+            path: DEFAULT_PATH.into(),
+            msg: "".into(),
+        }
     }
-    path.push_str("/XInput1_4.dll");
+}
+
+impl eframe::App for AutofireApp {
+    fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.horizontal(|ui| {
+                ui.text_edit_singleline(&mut self.path);
+                if ui.button("open folder").clicked() {
+                    println!("clicked");
+                }
+            });
+            if ui.button("patch").clicked() {
+                self.msg = match patch(DLL_PATH, &self.path) {
+                    Ok(_) => "successfully".to_owned(),
+                    Err(err) => err.to_string(),
+                };
+            }
+            if !self.msg.is_empty() {
+                ui.label(&self.msg);
+            }
+        });
+    }
+}
+
+fn patch(dll_path: &str, game_path: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let mut data = std::fs::read(dll_path)?;
+    let mut game_dll_path = game_path.to_owned();
+    game_dll_path.push_str("/XInput1_4.dll");
 
     let indexes = find_all(&data, &BYTECODE[0]);
-    if indexes.len() == 1 {
-        substitute(&mut data, &BYTECODE[1], indexes[0]);
-        std::fs::write(path, data)?;
+    match indexes.len() {
+        1 => {
+            substitute(&mut data, &BYTECODE[1], indexes[0]);
+            std::fs::write(game_dll_path, data)?;
+            Ok(())
+        }
+        0 => Err("pattern not found".into()),
+        _ => Err("multiple matches".into()),
     }
-    Ok(indexes.len())
 }
 
 fn main() {
-    match patch() {
-        Err(err) => println!("{}", err),
-        Ok(0) => println!("pattern not found"),
-        Ok(1) => println!("successfully"),
-        Ok(_) => println!("multiple matches"),
-    }
-    pause();
+    let options = eframe::NativeOptions {
+        viewport: egui::ViewportBuilder::default().with_inner_size([320.0, 240.0]),
+        ..Default::default()
+    };
+    eframe::run_native(
+        "Autofire",
+        options,
+        Box::new(|_cc| Box::<AutofireApp>::default()),
+    )
+    .unwrap();
 }
